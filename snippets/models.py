e26001f0c9e39111f623befe729e5121aaa48538
James <james@writeinthecode.com>
2019-07-26 13:04:24 -0500

    user admin roles and logging in

--- snippets/models.py
+++ snippets/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from pygments.lexers import get_all_lexer_by_name
 from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
+from pygments.lexers import get_lexer_by_name
 from pygments.formatters.html import HtmlFormatter
 from pygments import highlight
-from pygments.styles import get_all_styles
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
@@ -24,9 +24,9 @@ class Snippet(models.Model):
         ordering = ['created']
 
     def save(self, *args, **kwargs):
-        lexer = get_all_lexer_by_name(self.language)
+        lexer = get_lexer_by_name(self.language)
         linenos = 'table' if self.linenos else False
         options = {'title':self.title} if self.title else {}
         formatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
-        self.highlighted = highlight(self.code.lexer, formatter)
+        self.highlighted = highlight(self.code, lexer, formatter)
         super(Snippet, self).save(*args, **kwargs)
--- snippets/views.py
+++ snippets/views.py
@@ -1,48 +1,30 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-# from rest_framework import mixins
+from snippets.serializers import UserSerializer
 from rest_framework import generics
+from rest_framework import permissions
+from django.contrib.auth.models import User
+from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-
-### using Generics to simplify even farther, old code is commented ###
-# class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    
-    # def get(self, request, *args, **kwargs):       
-    #     return self.list(request, *args, **kwargs)
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
-    # def post(self, request, *args, **kwargs):
-    #     return self.create(request, *args, **kwargs)
-
-    # def delete(self, request, *args, **kwargs):
-    #     return self.destroy(request, *args, **kwargs)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
     
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer   
-# class SnippetDetail(APIView):
-#     def get_object(self, pk):
-#         try:
-#             snippet = Snippet.objects.get(pk=pk)
-#         except Snippet.DoesNotExist:
-#             raise Http404
+    serializer_class = SnippetSerializer  
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
-#     def get(self, request, pk, format=None):
-#         snippet = self.get_object(pk)
-#         serializer = SnippetSerializer(snippet)
-#         return Response(serializer.data)
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-#     def put(self, request, pk, format=None):
-#         snippet = self.get_object(pk)
-#         serializer = SnippetSerializer(snippet, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-#     def delete(self, request, pk, format=None):
-#         snippet = self.get_object(pk)
-#         snippet.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
